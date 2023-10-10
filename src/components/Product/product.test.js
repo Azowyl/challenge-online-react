0705f89aa6ac3945ae53a3487f2171e8277fd417
Facundo Gerstner <facundo.gerstner@tiendanube.com>
2023-10-10 09:15:03 -0300

    clean test by using snapshot testing

--- src/components/Product/product.test.js
+++ src/components/Product/product.test.js
@@ -7,8 +7,10 @@ import Product from "./Product";
 const data = { name: "T-shirt", count: 3, price: 20, promotionalPrice: 15 };
 
 describe("Render Product", () => {
+  let renderComponent;
+
   beforeEach(() =>
-    render(
+    renderComponent = () => render(
       <Product
         name={data.name}
         count={data.count}
@@ -18,18 +20,7 @@ describe("Render Product", () => {
     )
   );
 
-  it("Render Name", () => {
-    expect(screen.getByText(data.name)).toBeInTheDocument();
-  });
-  it("Render Count", () => {
-    expect(screen.getByText(data.count.toString())).toBeInTheDocument();
-  });
-  it("Render Price", () => {
-    expect(screen.getByText(data.price.toString())).toBeInTheDocument();
-  });
-  it("Render Promotional Price", () => {
-    expect(
-      screen.getByText(data.promotionalPrice.toString())
-    ).toBeInTheDocument();
-  });
+  it('renders Product correctly', () => {
+    expect(renderComponent().asFragment()).toMatchSnapshot();
+  })
 });
