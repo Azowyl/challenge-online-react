46c36b37882401bfa43299fadc8608a978b4d887
Facundo Gerstner <facundo.gerstner@tiendanube.com>
2023-10-10 09:12:29 -0300

    fix failing test

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./Product.scss";
 
-const Product = ({ name, count, price, key }) => {
+const Product = ({ name, count, price, promotionalPrice, key }) => {
   return (
     <div className="product" key={key}>
       <span>{name}</span>
       <span>{count}</span>
       <span>{price}</span>
+      <span>{promotionalPrice}</span>
     </div>
   );
 };
